e276b7e7673a4fbb7f8989de6c970f039e4b670b
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:48:40 -0600

    790.1

--- .freeCodeCamp/test/780.test.js
+++ .freeCodeCamp/test/780.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('You', () => {
@@ -8,4 +8,4 @@ describe('You', () => {
 
     assert(directoryContents.includes('bingo.sh') && lastCommand[0] === 'touch' && /bingo.sh/g.test(lastCommand[1]));
   });
-});
+});*/
