cf335e0f596230dedaf63ae14040952d3e5c8130
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:46:37 -0600

    760.1

--- .freeCodeCamp/test/760.test.js
+++ .freeCodeCamp/test/760.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should have the "sleep" command added correctly', async () => {
+  it('should have decrement the suggested variable added correctly', async () => {
     const scriptFile = await getFileContents('../countdown.sh');
 
     assert(false);
   });
-});*/
+});
