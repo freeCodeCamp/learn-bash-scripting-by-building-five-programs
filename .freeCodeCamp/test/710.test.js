612962a1a9c041e0c9584d5e34dd7ef58279fb42
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:43:21 -0600

    710.1

--- .freeCodeCamp/test/710.test.js
+++ .freeCodeCamp/test/710.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should have the suggested comment added correctly', async () => {
+  it('should have the suggested variable added correctly', async () => {
     const scriptFile = await getFileContents('../countdown.sh');
 
     assert(false);
   });
-});*/
+});
