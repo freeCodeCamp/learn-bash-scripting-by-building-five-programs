8e3d66dfa9eef7dd8b11f03a281c6c4b2b7bb4fa
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:26:19 -0600

    140.1

---
+++ .freeCodeCamp/test/140.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the "ls" command added correctly', async () => {
+    const scriptFile = await getFileContents('../questionnaire.sh');
+
+    assert(false);
+  });
+});
