b3b17a323ea99cb50fadcebb778370c7af36b643
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:11:43 -0600

    40.1

---
+++ .freeCodeCamp/test/40.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should run your script with the "bash" command', async () => {
+    const cwd = await getCwd();
+    const lastCommand = await getLastCommand();
+
+    assert(lastCommand[0] === 'bash' && lastCommand[1] === 'questionnaire.sh' && cwd === '/home/strove/project');
+  });
+});
