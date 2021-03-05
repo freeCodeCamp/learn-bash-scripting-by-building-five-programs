7c6967c7b4af4beb060ab463e229d3fc1245b87b
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:46:06 -0600

    750.1

--- .freeCodeCamp/test/750.test.js
+++ .freeCodeCamp/test/750.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should have the suggested "while" loop added correctly', async () => {
+  it('should have the "sleep" command added correctly', async () => {
     const scriptFile = await getFileContents('../countdown.sh');
 
     assert(false);
   });
-});*/
+});
