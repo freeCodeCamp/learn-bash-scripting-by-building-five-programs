dc447818e6b4bab7cafca4e39e44fd94827b8389
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:36:59 -0600

    590.1

--- .freeCodeCamp/test/590.test.js
+++ .freeCodeCamp/test/590.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should have the suggested condition for your "if" statement', async () => {
+  it('should have the suggested "else" statement', async () => {
     const scriptFile = await getFileContents('../countdown.sh');
 
     assert(false);
   });
-});*/
+});
