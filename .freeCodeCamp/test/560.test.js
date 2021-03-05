df2dfc17479b55d91b0b5c41298c419f299c4a88
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:33:33 -0600

    560.1

---
+++ .freeCodeCamp/test/560.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested condition for your "if" statement', async () => {
+    const scriptFile = await getFileContents('../countdown.sh');
+
+    assert(false);
+  });
+});
