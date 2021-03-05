1fb493d332edaaddc842973d800a16dd5f9404eb
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:32:06 -0600

    210.1

--- .freeCodeCamp/test/200.test.js
+++ .freeCodeCamp/test/200.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents, appendAndRun } = require('./utils');
 
 describe('Your script', () => {
@@ -8,4 +8,4 @@ describe('Your script', () => {
 
     assert(/^\s*read[ \t]+NAME[ \t]*;?\s*$/gm.test(scriptFile) && /^me$/gm.test(output));
   });
-});
+});*/
