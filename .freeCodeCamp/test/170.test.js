cf40007c17310a18a95a2b14930fa98146c928e7
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:27:33 -0600

    170.1

--- .freeCodeCamp/test/170.test.js
+++ .freeCodeCamp/test/170.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const { appendAndRun } = require('./utils');
 
 describe('Your script', () => {
-  it('should "echo" the suggested variable correctly', async () => {
+  it('should have the suggested variable added correctly', async () => {
     const output = await appendAndRun('../questionnaire.sh', 'echo $QUESTION1');
 
     assert(/^What's your name\?$/gm.test(output));
