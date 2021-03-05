db179af428c1e6646099c451e79fe0ca38298fe0
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:27:34 -0600

    470.1

--- .freeCodeCamp/test/470.test.js
+++ .freeCodeCamp/test/470.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
   it('should enter the suggested command in the terminal', async () => {
     const lastCommand = await getLastCommand();
 
-    assert(lastCommand[0] === 'help' && (lastCommand[1] === undefined || lastCommand[1].op === ';'));
+    assert(lastCommand[0] === 'man' && lastCommand[1] === 'if' && (lastCommand[2] === undefined || lastCommand[2].op === ';'));
   });
-});*/
+});
