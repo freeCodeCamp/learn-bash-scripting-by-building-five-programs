27c4f17509bf793bafd349f56b1c2e835f15bb95
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:03:27 -0600

    20.1

---
+++ .freeCodeCamp/test/20.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand, getDirectoryContents, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should use the "touch" command to create the correct file', async () => {
+    const cwd = await getCwd();
+    const directoryContents = await getDirectoryContents(`..`);
+    const lastCommand = await getLastCommand();
+
+    assert(directoryContents.includes('questionnaire.sh') && lastCommand[0] === 'touch' && /questionnaire\.sh/g.test(lastCommand[1]) && cwd === '/home/strove/project');
+  });
+});
