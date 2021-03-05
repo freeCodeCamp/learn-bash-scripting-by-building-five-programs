0fe1249358404aadbed8fee72585dfc09a505465
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:37:30 -0600

    610.1

--- .freeCodeCamp/test/610.test.js
+++ .freeCodeCamp/test/610.test.js
@@ -1,11 +1,12 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('You', () => {
   it('should run your script with the correct argument', async () => {
     const cwd = await getCwd();
     const lastCommand = await getLastCommand();
+    const arg = parseInt(lastCommand[1]);
 
-    assert(lastCommand[0] === './countdown.sh' && lastCommand[1] === '1' && cwd === '/home/strove/project');
+    assert(lastCommand[0] === './countdown.sh' && (arg < 1 || isNaN(arg) || /\./.test(lastCommand[1])) && cwd === '/home/strove/project');
   });
-});*/
+});
