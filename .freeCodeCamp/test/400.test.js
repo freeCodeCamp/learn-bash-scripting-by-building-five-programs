908aa6d2d698672d8b3b4290fcd1b60fd9641456
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:41:36 -0600

    400.1

---
+++ .freeCodeCamp/test/400.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the correct "shebang"', async () => {
+    const scriptFile = await getFileContents('../countdown.sh');
+
+    assert(/^\s*#![ \t]*\/bin\/bash\s*$/gm.test(scriptFile));
+  });
+});
