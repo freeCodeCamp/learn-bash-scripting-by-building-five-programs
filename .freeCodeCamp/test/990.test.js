fb9f6cb679123fac275c46e895204c8e24fc84aa
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 22:06:25 -0600

    990.1

--- .freeCodeCamp/test/990.test.js
+++ .freeCodeCamp/test/990.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should have the suggested "elif" statement added correctly', async () => {
+  it('should have the suggested "else" statement added correctly', async () => {
     const scriptFile = await getFileContents('../bingo.sh');
 
     assert(false);
   });
-});*/
+});
