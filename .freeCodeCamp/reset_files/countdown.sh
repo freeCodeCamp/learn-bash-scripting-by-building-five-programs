8116f00080ccfb6213492824b00b68ed42f0dc46
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:43:21 -0600

    710.1

--- .freeCodeCamp/reset_files/countdown.sh
+++ .freeCodeCamp/reset_files/countdown.sh
@@ -6,11 +6,13 @@ echo -e "\n~~ Countdown Timer ~~\n"
 
 if [[ $1 -gt 0 ]]
 then
+  : '
   for (( i = $1; i >= 0; i-- ))
   do
     echo $i
     sleep 1
   done
+  '
 else
   echo Include a positive integer as the first argument.
 fi
