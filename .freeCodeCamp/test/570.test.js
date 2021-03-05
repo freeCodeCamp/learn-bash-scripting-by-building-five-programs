5861b75e57eada90bccb7446f9c7eb3df6589a38
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 21:35:58 -0600

    570.1

---
+++ .freeCodeCamp/test/570.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const lastCommand = await getLastCommand();
+    const lC = lastCommand;
+    const correctLastArg = lC[3] === '$?' || lC[3] === '$?;'
+
+    assert(lC[0] === 'ls' && lC[1] === '-y;' && lC[2] === 'echo' && correctLastArg);
+  });
+});
