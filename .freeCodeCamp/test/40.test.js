ea8f518b9c3b7e47205db1bcb9d4c1891f50f6b8
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:11:43 -0600

    40.1

--- .freeCodeCamp/test/40.test.js
+++ .freeCodeCamp/test/40.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -6,6 +6,6 @@ describe('You', () => {
     const cwd = await getCwd();
     const lastCommand = await getLastCommand();
 
-    assert(lastCommand[0] === 'sh' && /^(\.\/)?questionnaire\.sh$/gm.test(lastCommand[1]) && /\/project$/.test(cwd));
+    assert(lastCommand[0] === 'bash' && /^(\.\/)?questionnaire\.sh$/gm.test(lastCommand[1]) && /\/project$/.test(cwd));
   });
-});*/
+});
