395cac83bcd0158feeed6f5b52ad2d1ee1da1048
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-04 19:32:06 -0600

    210.1

--- .freeCodeCamp/test/210.test.js
+++ .freeCodeCamp/test/210.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should use "read" to get input into the correct variable', async () => {
+  it('should "echo" the suggested text correctly', async () => {
     const scriptFile = await getFileContents('../questionnaire.sh');
 
     assert(false);
   });
-});*/
+});
