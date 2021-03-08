cf39900c28a47ca424e298d4427d9767021fd834
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-07 23:24:34 -0600

    584.1

---
+++ .freeCodeCamp/test/584.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const lastCommand = await getLastCommand();
+    const lC = lastCommand;
+    const correctFile = lC[2] === 'countdown.sh' || lC[2] === './countdown.sh';
+    const first = lC[0] === '[[' && lC[1] === '-x' && correctFile;
+    const second = lC[4] === '5' && lC[5] === '-lt' && lC[6] === '0' && lC[7] === ']];'
+    const correctLastArg = lC[9] === '$?' || lC[9] === '$?;'
+
+    assert(first && lC[3] === '&&' && second && lC[8] === 'echo' && correctLastArg);
+  });
+});
