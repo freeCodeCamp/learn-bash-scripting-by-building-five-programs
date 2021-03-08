7b67a46b0dbc32778bff340a9ec2557d6d36d501
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-07 22:22:27 -0600

    505.1

--- .freeCodeCamp/test/505.test.js
+++ .freeCodeCamp/test/505.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -6,6 +6,6 @@ describe('You', () => {
     const cwd = await getCwd();
     const lastCommand = await getLastCommand();
 
-    assert(lastCommand[0] === './countdown.sh' && lastCommand[1] === 'arg1' && /\/project$/.test(cwd));
+    assert(lastCommand[0] === './countdown.sh' && lastCommand[1] !== 'arg1' && /\/project$/.test(cwd));
   });
-});*/
+});
