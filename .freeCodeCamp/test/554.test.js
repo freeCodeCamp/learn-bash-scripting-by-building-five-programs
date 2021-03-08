4aa9362708b78491e055f222902f5d4826198753
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-07 22:36:48 -0600

    554.1

---
+++ .freeCodeCamp/test/554.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const lastCommand = await getLastCommand();
+
+    assert(lastCommand[0] === '[[' && lastCommand[1] === '0' && lastCommand[2] === '-le' && lastCommand[3] === '5' && lastCommand[4] === ']]');
+  });
+});
