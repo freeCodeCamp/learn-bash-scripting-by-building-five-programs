6f0f9eccdb517468c6044f4b0ccf6457eb2af015
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-07 23:27:18 -0600

    588.1

--- .freeCodeCamp/test/586.test.js
+++ .freeCodeCamp/test/586.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -7,4 +7,4 @@ describe('You', () => {
 
     assert(/^\s*\[\[\s+-x\s+(\.\/)?countdown\.sh\s+\|\|\s+5\s+-le\s+4\s+\]\]\s*;\s*echo\s+\$\?\s*;?\s*$/g.test(lastCommand));
   });
-});
+});*/
