76687d8eb95457c1e9c5b6e364f983ef623f0050
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-10 14:12:57 -0600

    692.1

---
+++ .freeCodeCamp/test/692.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the correct "for" loop', async () => {
+    const scriptFile = await getFileContents('../countdown.sh');
+
+    assert(false);
+  });
+});
