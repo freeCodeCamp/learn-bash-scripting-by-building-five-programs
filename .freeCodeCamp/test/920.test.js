d8dc82cb7b303ea51fa9aa0a2ac1b3feb7e54056
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 13:10:18 -0600

    920.1

---
+++ .freeCodeCamp/test/920.test.js
@@ -0,0 +1,11 @@
+describe('You', () => {
+  it('should enter the suggested command in the terminal', async () => {
+    const lastCommand = await getLastCommand();
+    const joined = lastCommand.join('');
+    const joinedTest = joined === 'NUM=$((RANDOM%75+1))' || joined === 'NUM=$((RANDOM%75+1));';
+    const first = lastCommand.find(str => /NUM=\$\(\(/.test(str));
+    const second = lastCommand.find(str => /\)\)/.test(str));
+
+    assert(joinedTest && first && second);
+  });
+});
