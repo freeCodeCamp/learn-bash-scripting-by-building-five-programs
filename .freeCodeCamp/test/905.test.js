815e14d0710c7c0470b1d3f91e003df0de7c440c
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 12:19:52 -0600

    905.1

--- .freeCodeCamp/test/905.test.js
+++ .freeCodeCamp/test/905.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
   it('should enter the suggested command in the terminal', async () => {
     const lastCommand = await getLastCommand(0, false);
 
-    assert(/^\s*echo\s+('|"|)\$I\1\s*;?\s*$/.test(lastCommand));
+    assert(/^\s*J=('|"|)\$\(\(\s*\$?I\s*\-\s*6\s*\)\)\s*\1\s*;?\s*$/.test(lastCommand));
   });
-});*/
+});
