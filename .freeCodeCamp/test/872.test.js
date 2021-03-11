9e4bc329c3f62b5d5416de1be920cce096508c32
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 11:30:58 -0600

    875.1

--- .freeCodeCamp/test/872.test.js
+++ .freeCodeCamp/test/872.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
 
     assert(joinedTest && first && second);
   });
-});
+});*/
---
+++ .freeCodeCamp/test/875.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command in the terminal', async () => {
+    const lastCommand = await getLastCommand();
+    const secondToLastCommand = await getLastCommand(1);
+    const slc = secondToLastCommand;
+    const slcTest = slc[0] === '((' && slc[1] === '$RANDOM' && slc[2] === '%' && slc[3] === '75' && slc[4] === '))' && slc[5] === undefined;
+
+    assert(lastCommand[0] === 'echo' && lastCommand[1] === '$?' && lastCommand[2] === undefined && slcTest);
+  });
+});
