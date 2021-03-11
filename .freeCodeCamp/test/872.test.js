cae6ef716d376096307b391ef2fa7d0fda96add7
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 11:27:50 -0600

    872.1

--- .freeCodeCamp/test/872.test.js
+++ .freeCodeCamp/test/872.test.js
@@ -1,14 +1,14 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
   it('should enter the suggested command in the terminal', async () => {
     const lastCommand = await getLastCommand();
-    const math = lastCommand.splice(1).join('');
-    const mathTest = math === '(($RANDOM%75))' || math === '(($RANDOM%75))';
+    const joined = lastCommand.join('');
+    const joinedTest = joined === '(($RANDOM%75))' || joined === '(($RANDOM%75))';
     const first = lastCommand.find(str => /\(\(/.test(str));
     const second = lastCommand.find(str => /\)\)/.test(str));
 
-    assert(lastCommand[0] === 'echo' && mathTest && first && second);
+    assert(joinedTest && first && second);
   });
-});*/
+});
