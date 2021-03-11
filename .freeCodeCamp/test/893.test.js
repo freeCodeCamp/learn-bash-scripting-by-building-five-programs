7a1a4462354cd89ecb26b08a98921d58dab9616b
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 12:05:05 -0600

    893.1

---
+++ .freeCodeCamp/test/893.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command in the terminal', async () => {
+    const lastCommand = await getLastCommand();
+
+    const first = lastCommand.find(str => /\(\(/.test(str));
+    const second = lastCommand.find(str => /\+=/.test(str));
+    const third = lastCommand.find(str => /\)\)/.test(str));
+
+    assert(lastCommand[0] === '((I+=10))' && first && second && third);
+  });
+});
