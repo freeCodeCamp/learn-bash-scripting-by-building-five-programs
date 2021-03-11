c01be807caa3dd6c0be3cd11c657b1821fd64457
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 13:13:17 -0600

    928.1

--- .freeCodeCamp/test/920.test.js
+++ .freeCodeCamp/test/920.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -7,4 +7,4 @@ describe('You', () => {
 
     assert(/^\s*echo\s+('|"|)\$\(\(\s*\$?RANDOM\s*%\s*75\s*\+\s*1\s*\)\)\1\s*;?\s*$/.test(lastCommand));
   });
-});
+});*/
