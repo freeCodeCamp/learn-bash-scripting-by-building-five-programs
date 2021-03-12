e07ad1083480dcda9ffa0ffa4af6e5d6ae07d914
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-12 11:29:36 -0600

    1440.1

--- .freeCodeCamp/test/1430.test.js
+++ .freeCodeCamp/test/1430.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -9,4 +9,4 @@ describe('You', () => {
 
     assert(directoryContents.includes('five.sh') && lastCommand[0] === 'touch' && /five.sh/g.test(lastCommand[1]) && cwd === '/home/strove/project');
   });
-});
+});*/
