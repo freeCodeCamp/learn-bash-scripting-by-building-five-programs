061332df3d80fc7e2268c3dd07805350f79c4536
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-12 11:28:34 -0600

    1410.1

---
+++ .freeCodeCamp/test/1410.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getFileContents, appendAndRun } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggests "if" statement added correctly', async () => {
+    const scriptFile = await getFileContents('../fortune.sh');
+    const output = await appendAndRun('../fortune.sh', 'GET_FORTUNE again', { input: ['test?', 'test?'] });
+
+    assert(/^\s*if[ \t]+\[\[[ \t]+![ \t]+\$1[ \t]+\]\]/gm.test(scriptFile) && /Ask a yes or no question/g.test(output) && /Try again\. Make sure it ends with a question mark\./g.test(output));
+  });
+});
