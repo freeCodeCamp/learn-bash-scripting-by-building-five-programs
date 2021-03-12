a30b2f4f06b73155bedf1c484b90d8f26a2aeea8
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-12 11:29:20 -0600

    1430.1

--- .freeCodeCamp/test/1428.test.js
+++ .freeCodeCamp/test/1428.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -8,4 +8,4 @@ describe('You', () => {
 
     assert(lastCommand[0] === './fortune.sh' && /\/project$/.test(cwd));
   });
-});
+});*/
