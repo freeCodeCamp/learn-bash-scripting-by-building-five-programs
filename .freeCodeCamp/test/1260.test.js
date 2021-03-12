6af6268bf0affea1b03f33ee32808a370f11e638
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 21:08:35 -0600

    1260.1

---
+++ .freeCodeCamp/test/1260.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggested "until" loop added correctly', async () => {
+    const scriptFile = await getFileContents('../fortune.sh');
+
+    assert(false);
+  });
+});
