1a661e4a68a7272e317b6696ecfa1bab8f563e61
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 21:03:03 -0600

    1200.1

--- .freeCodeCamp/reset_files/fortune.sh
+++ .freeCodeCamp/reset_files/fortune.sh
@@ -8,7 +8,7 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 
 GET_FORTUNE() {
-
+  echo Ask a yes or no question:
 }
 
 echo ${RESPONSES[$N]}
