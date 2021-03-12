fce8d276c43985e3e28fb3343599aec2247f7043
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 23:12:08 -0600

    1360.1

--- .freeCodeCamp/test/1350.test.js
+++ .freeCodeCamp/test/1350.test.js
@@ -1,11 +1,13 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCommandOutput } = require('./utils');
 
 describe('You', () => {
   it('should enter the suggested command in the terminal', async () => {
     const lastCommand = await getLastCommand(0, false);    
-    const output = await getCommandOutput(`${lastCommand}\ndeclare -p VAR`)
+    const output = await getCommandOutput(`${lastCommand}
+declare -p VAR`)
 
-    assert(/^VAR=('|")hello world$1/.test(lastCommand) && output === 'declare -- VAR="hello world"\n');
+    assert(/^VAR=('|")hello world$1/.test(lastCommand) && output === 'declare -- VAR="hello world"
+');
   });
-});
+});*/
---
+++ .freeCodeCamp/test/1360.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getLastCommand, getCommandOutput } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command in the terminal', async () => {
+    const lastCommand = await getLastCommand();
+
+    assert(lastCommand[0] === 'echo' && (lastCommand[1] === '$VAR' || lastCommand[1] === '$VAR;'));
+  });
+});
