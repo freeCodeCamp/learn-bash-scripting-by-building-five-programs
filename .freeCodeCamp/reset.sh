9c6ada0394fa208f56c957d86b13e4f3eb767a91
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-12 11:29:36 -0600

    1440.1

--- .freeCodeCamp/reset.sh
+++ .freeCodeCamp/reset.sh
@@ -2,3 +2,4 @@ cp ./.freeCodeCamp/reset_files/questionnaire.sh .
 cp ./.freeCodeCamp/reset_files/countdown.sh .
 cp ./.freeCodeCamp/reset_files/bingo.sh .
 cp ./.freeCodeCamp/reset_files/fortune.sh .
+cp ./.freeCodeCamp/reset_files/five.sh .
