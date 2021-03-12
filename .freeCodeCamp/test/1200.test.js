a0f13e62ae70429822758652dc03594c948b5265
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 21:03:03 -0600

    1200.1

--- .freeCodeCamp/test/1200.test.js
+++ .freeCodeCamp/test/1200.test.js
@@ -1,10 +1,10 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your script', () => {
-  it('should "echo" the suggested text correctly', async () => {
+  it('should call the suggested fuction correctly', async () => {
     const scriptFile = await getFileContents('../fortune.sh');
 
     assert(false);
   });
-});*/
+});
