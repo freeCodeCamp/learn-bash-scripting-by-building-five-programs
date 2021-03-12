e360de749a2ac1b60ccbed5149f6a8cb07e9b865
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-11 21:02:34 -0600

    1190.1

--- .freeCodeCamp/reset_files/fortune.sh
+++ .freeCodeCamp/reset_files/fortune.sh
@@ -7,4 +7,8 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
 
+GET_FORTUNE() {
+
+}
+
 echo ${RESPONSES[$N]}
