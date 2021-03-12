35a4a99096b847693593d3b1957fce5f6dd7074e
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-12 11:28:34 -0600

    1410.1

---
+++ .freeCodeCamp/test/1410.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script', () => {
+  it('should have the suggests "if" statement added correctly', async () => {
+    const scriptFile = await getFileContents('../fortune.sh');
+
+    assert(false);
+  });
+});
