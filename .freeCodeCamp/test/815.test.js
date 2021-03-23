d3d76b64098a8a5cf459b7b0e033fe77dd44267f
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-23 08:22:20 -0500

    817.1

--- .freeCodeCamp/test/815.test.js
+++ .freeCodeCamp/test/815.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getCommandOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/^\n~~ Bingo Number Generator ~~\n{2}/g.test(output));
   });
-});
+});*/
