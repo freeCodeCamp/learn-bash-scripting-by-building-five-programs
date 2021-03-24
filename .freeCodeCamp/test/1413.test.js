3180413f301755f8a60f559a37145b3841c77126
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-24 17:04:08 -0500

    1413.1

--- .freeCodeCamp/test/1413.test.js
+++ .freeCodeCamp/test/1413.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getFileContents, appendAndRun } = require('./utils');
 
 describe('Your script', () => {
@@ -6,6 +6,6 @@ describe('Your script', () => {
     const scriptFile = await getFileContents('../fortune.sh');
     const output = await appendAndRun('../fortune.sh', 'GET_FORTUNE again', { input: ['test?', 'test?'] });
 
-    assert(/^\s*if[ \t]+\[\[[ \t]+![ \t]+\$1[ \t]+\]\]/gm.test(scriptFile) && /Ask a yes or no question/g.test(output) && /Try again\. Make sure it ends with a question mark\./g.test(output));
+    assert(false);
   });
-});*/
+});
