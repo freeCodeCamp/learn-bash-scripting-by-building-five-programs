1184069a778eec474e2ba59ed678b02190d6ba54
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-24 20:38:57 -0500

    1428.1

--- .freeCodeCamp/test/1425.test.js
+++ .freeCodeCamp/test/1425.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getScriptOutput } = require('./utils');
 
 describe('Your script', () => {
@@ -7,4 +7,4 @@ describe('Your script', () => {
 
     assert(/\n{2}(Yes|No|Maybe|Outlook good|Don't count on it|Ask again later)\n$/g.test(output))
   });
-});
+});*/
