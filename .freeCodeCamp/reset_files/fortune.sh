a79310bf6a098a6012ec66d697a107aea2519ff8
moT01 <20648924+moT01@users.noreply.github.com>
2021-03-24 20:35:18 -0500

    1412.1

--- .freeCodeCamp/reset_files/fortune.sh
+++ .freeCodeCamp/reset_files/fortune.sh
@@ -7,8 +7,12 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
 
-GET_FORTUNE() {
-  echo Ask a yes or no question:
+function GET_FORTUNE() {
+  if [[ ! $1 ]]
+  then
+    echo Ask a yes or no question:
+  fi
+
   read QUESTION
 }
 
