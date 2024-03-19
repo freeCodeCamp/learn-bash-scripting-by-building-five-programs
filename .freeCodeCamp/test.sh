fb5d857f72e31914947a7beedc644ee24caae16d
Jonathan Golden <jonathan.lgolden@gmail.com>
2024-03-19 19:01:19 +0000

    Save progress: 230.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 QUESTION1="What's your name?";
 echo $QUESTION1
-read NAME
+read NAME;
+echo Hello $NAME.;
+QUESTION2="Where are you from?";
+echo $QUESTION2
 
-echo $NAME
+
+
+echo $QUESTION2
--- questionnaire.sh
+++ questionnaire.sh
@@ -3,3 +3,7 @@ QUESTION1="What's your name?";
 echo $QUESTION1
 read NAME;
 echo Hello $NAME.;
+QUESTION2="Where are you from?";
+echo $QUESTION2
+
+
