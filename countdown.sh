2cbd6991f4bcb1146a68e264d6d1b0ec1823ea77
Soham Kumar Modi <soham3852@gmail.com>
2024-08-19 10:53:43 +0000

    Save progress: 700.1

--- countdown.sh
+++ countdown.sh
@@ -5,11 +5,11 @@ echo -e "\n~~ Countdown Timer ~~\n"
 
 if [[ $1 -gt 0 ]]
 then
-  for (( i = $1; i >= 0; i-- ))
+: '  for (( i = $1; i >= 0; i-- ))
   do
     echo $i
     sleep 1
-  done
+  done '
 else
   echo Include a positive integer as the first argument.
 fi
