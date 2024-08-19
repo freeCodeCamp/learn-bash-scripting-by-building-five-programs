8e8fe1448d890152d5d86d4802593de4c922d656
Soham Kumar Modi <soham3852@gmail.com>
2024-08-19 07:07:05 +0000

    Save progress: 530.1

--- countdown.sh
+++ countdown.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#Program that counts down to zero from a given argument
 
-if [[ $1 == arg1 ]]
-then 
+# Program that counts down to zero from a given argument
+
+if [[ $1 -lt 5 ]]
+then
   echo true
 else
   echo false
--- questionnaire.sh
+++ questionnaire.sh
@@ -3,25 +3,16 @@
 echo -e "\n~~ Questionnaire ~~\n"
 
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
 
 echo $QUESTION1
 read NAME
 
-echo Hello $NAME.
-
-QUESTION2="Where are you from?"
-
 echo $QUESTION2
 read LOCATION
 
-echo Hello $NAME from $LOCATION.
-
-QUESTION3="What's your favorite coding website?"
-
 echo $QUESTION3
-
 read WEBSITE
 
-echo -e "\n"
-echo Hello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!
-
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
