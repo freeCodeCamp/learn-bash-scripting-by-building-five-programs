8027e13a0b039d2952bd592b7bcfeaa764b98b94
AbiramiR-27 <abiramesh2023@gmail.com>
2026-06-06 00:38:54 +0530

    fix(curriculum): update step 170 test description to give valid hint

--- .freeCodeCamp/test/170.test.js
+++ .freeCodeCamp/test/170.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const { appendAndRun } = require('./utils');
 
 describe('Your script', () => {
-  it('should "echo" the suggested variable correctly', async () => {
+  it('should define the variable and its value correctly', async () => {
     const output = await appendAndRun('../questionnaire.sh', 'echo $QUESTION1');
 
     assert(/^What's your name\?$/gm.test(output));
